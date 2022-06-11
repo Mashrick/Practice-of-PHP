3566c5f5d5bbbf8d3fb3001eac59a940d648557f
Md. Mashrickul Haque <mashu.haque@gmail.com>
2022-06-12 02:03:46 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,47 @@
+
+
+
+<<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Practice</title>
+</head>
+<body>
+
+
+
+		<?php 
+
+			$x = 23;
+			$c = 6;
+			$z = 3;
+
+
+			if ($x>$c && $x>$z) {
+				echo "x is big = ".$x;
+			} 
+			elseif ($c>$x && $c>$z) {
+				echo "c is big = ".$c;
+			}
+			else {
+				echo "z is big = ".$z;
+			}
+
+			
+		
+		?>
+
+
+
+</body>
+</html>
+
+
+
+
+
+
+
+
