e29836b9862c23c465c80d8aaf85a9d1071e1555
Md. Mashrickul Haque <mashu.haque@gmail.com>
2022-06-12 02:49:23 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 
 
 
-<<!DOCTYPE html>
+<!DOCTYPE html>
 <html>
 <head>
 	<meta charset="utf-8">
@@ -10,36 +10,62 @@
 </head>
 <body>
 
-	<!--	find the largest number between three numbers using if...elif...else	-->
 
-		<?php 
+		<!--	find the largest number between three numbers using if...elif...else	-->
 
-			$x = 23;
-			$c = 6;
-			$z = 3;
+	<?php 
+
+		$x = 23;
+		$c = 6;
+		$z = 3;
 
 
 			if ($x>$c && $x>$z) {
-				echo "x is big = ".$x;
+				echo "X is big = " .$x. ".";
 			} 
 			elseif ($c>$x && $c>$z) {
-				echo "c is big = ".$c;
+				echo "C is big = " .$c. ".";
 			}
 			else {
-				echo "z is big = ".$z;
+				echo "Z is big = " .$z. ".";
+			}
+		
+
+
+	?>
+
+
+	<br>
+	<br>
+
+		<!--	find the largest number between Two numbers using if...elif...else	-->
+
+	<?php 
+
+		$x = 43;
+		$i = 43;
+
+			if ($x>$i) {
+
+				echo "X = " .$x. "is big";
+
+			}elseif ($x<$i) {
+
+				echo "I = " .$i. "is big";
+
+			} else {
+
+				echo "X = " .$x. " & I = " .$i. " Both numbers are SAME.";				
 			}
 
-			
+
+	?>
+
 		
-		?>
-
-
+		
 
 </body>
 </html>
-
-
-
 
 
 
