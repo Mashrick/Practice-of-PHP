a2fe082da687e9758481d48850f59dce56febcda
Md. Mashrickul Haque <mashu.haque@gmail.com>
2022-06-12 15:46:33 +0600

    Update index.php

--- index.php
+++ index.php
@@ -152,6 +152,61 @@
 
 
 	?>
+	
+	
+	
+	<br>
+	<br>
+	
+	
+	
+	<!--	 GPA Calculator	using if...elif...else		-->
+
+	<?php 
+
+
+		echo "GPA Calculator","<br><br>";
+
+
+		$marks= 67; 	// Change Grade Number here
+
+			if ($marks >= 80 && $marks <= 100) {
+
+				echo "Grade A+";
+
+			}elseif ($marks >= 70 && $marks <= 79) {
+
+				echo "Grade A";
+
+			}elseif ($marks >= 60 && $marks <= 69) {
+
+				echo "Grade A-";
+
+			}elseif ($marks >= 50 && $marks <= 59) {
+
+				echo "Grade B";
+
+			}elseif ($marks >= 40 && $marks <= 49) {
+
+				echo "Grade C";
+
+			}elseif ($marks >= 33 && $marks <= 39) {
+
+				echo "Grade D";
+
+			}elseif ($marks >=  0 && $marks <= 32) {
+
+				echo "Grade F";
+
+			} else {
+
+				echo "Invalid";
+
+			}
+
+
+
+	?>
 
 		
 		
